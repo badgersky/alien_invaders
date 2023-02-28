68e93a124d82a9690f89aea65ee5eb8423e924f0
badgersky <szymonek.ptasznik@gmail.com>
2023-02-28 19:40:04 +0100

    moved spaceship class to different file

--- alien_invaders.py
+++ alien_invaders.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import settings as s
+from spaceship import Spaceship
 
 
 def video_init():
@@ -37,32 +38,6 @@ def check_key_pressed_events(spaceship):
         spaceship.move(left=True)
     if keys[pygame.K_RIGHT]:
         spaceship.move(right=True)
-
-
-class Spaceship:
-
-    def __init__(self, screen):
-        self.image = self.load_image_of_spaceship()
-        self.spaceship_rect = self.image.get_rect()
-        self.screen = screen
-        self.screen_rect = screen.get_rect()
-        self.speed = s.SPACESHIP_SPEED
-
-        self.spaceship_rect.midbottom = self.screen_rect.midbottom
-        self.spaceship_x_float = float(self.spaceship_rect.x)
-
-    def move(self, right=False, left=False):
-        if right and self.spaceship_rect.right < self.screen_rect.right:
-            self.spaceship_x_float += self.speed
-            self.spaceship_rect.x = self.spaceship_x_float
-        if left and self.spaceship_rect.left > 0:
-            self.spaceship_x_float -= self.speed
-            self.spaceship_rect.x = self.spaceship_x_float
-
-    @staticmethod
-    def load_image_of_spaceship():
-        image = pygame.image.load('images/spaceship.bmp')
-        return image
 
 
 if __name__ == '__main__':
---
+++ spaceship.py
@@ -0,0 +1,28 @@
+import pygame
+import settings as s
+
+
+class Spaceship:
+
+    def __init__(self, screen):
+        self.image = self.load_image_of_spaceship()
+        self.spaceship_rect = self.image.get_rect()
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.speed = s.SPACESHIP_SPEED
+
+        self.spaceship_rect.midbottom = self.screen_rect.midbottom
+        self.spaceship_x_float = float(self.spaceship_rect.x)
+
+    def move(self, right=False, left=False):
+        if right and self.spaceship_rect.right < self.screen_rect.right:
+            self.spaceship_x_float += self.speed
+            self.spaceship_rect.x = self.spaceship_x_float
+        if left and self.spaceship_rect.left > 0:
+            self.spaceship_x_float -= self.speed
+            self.spaceship_rect.x = self.spaceship_x_float
+
+    @staticmethod
+    def load_image_of_spaceship():
+        image = pygame.image.load('images/spaceship.bmp')
+        return image
