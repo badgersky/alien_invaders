1fe0cfa1a2271f6c73ad1d4bce3a300c5972b375
badgersky <szymonek.ptasznik@gmail.com>
2023-02-28 19:08:12 +0100

    implemented spaceship movement, changed settings

--- alien_invaders.py
+++ alien_invaders.py
@@ -17,62 +17,40 @@ def main_loop():
     spaceship = Spaceship(screen)
 
     while True:
-        check_events(spaceship)
+        check_events()
 
-        spaceship.blit_spaceship()
-        pygame.display.flip()
+        spaceship.move()
+        screen.blit(spaceship.image, spaceship.spaceship_rect)
+        pygame.display.update()
         screen.fill(color=s.SCREEN_COLOR)
 
 
-def check_events(spaceship):
+def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, spaceship)
 
 
-def check_keydown_events(event, spaceship):
-    if event.type == pygame.K_RIGHT:
-        spaceship.move(right=True)
-    if event.type == pygame.K_LEFT:
-        spaceship.move(left=True)
+def load_image_of_spaceship():
+    image = pygame.image.load('images/spaceship.bmp')
+    return image
 
 
 class Spaceship:
+
     def __init__(self, screen):
+        self.image = load_image_of_spaceship()
+        self.spaceship_rect = self.image.get_rect()
         self.screen = screen
-        self.image = pygame.image.load('images/spaceship.bmp')
-        self.spaceship = self.image.get_rect()
-        screen_rect = self.screen.get_rect()
-        self.spaceship.midbottom = screen_rect.midbottom
-        self._moving_right = False
-        self._moving_left = False
+        self.screen_rect = screen.get_rect()
+        self.speed = s.SPACESHIP_SPEED
 
-    @property
-    def moving_right(self):
-        return self._moving_right
+        self.spaceship_rect.midbottom = self.screen_rect.midbottom
 
-    @moving_right.setter
-    def moving_right(self, is_moving):
-        self._moving_right = is_moving
-
-    @property
-    def moving_left(self):
-        return self._moving_left
-
-    @moving_left.setter
-    def moving_left(self, is_moving):
-        self._moving_left = is_moving
-
-    def blit_spaceship(self):
-        self.screen.blit(self.image, self.spaceship)
-
-    def move(self, right=False, left=False):
-        if right:
-            self.spaceship.x += s.SPACESHIP_SPEED
-        if left:
-            self.spaceship.x -= s.SPACESHIP_SPEED
+    def move(self):
+        self.spaceship_rect = self.spaceship_rect.move(self.speed, 0)
+        if self.spaceship_rect.right > self.screen_rect.right:
+            self.spaceship_rect.left = 0
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 SCREEN_SIZE = {
-    'width': 1080,
-    'height': 720,
+    'width': 1920,
+    'height': 1000,
 }
 
 SCREEN_COLOR = (60, 68, 60)
