2ffeda0b1b1b885ea980fe99f432498ce0ba4dd7
badgersky <szymonek.ptasznik@gmail.com>
2023-03-26 19:56:29 +0200

    created window for game menu

---
+++ menu.py
@@ -0,0 +1,32 @@
+import sys
+import pygame as p
+import settings as s
+
+
+class MainMenu:
+
+    def __init__(self):
+        self.screen = p.display.set_mode((1080, 720))
+        p.display.set_caption('Empire Menu')
+
+    def main_loop(self):
+        while True:
+            self.check_events()
+            self.screen.fill(color=s.SCREEN_COLOR)
+            p.display.flip()
+
+    def check_events(self):
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                sys.exit()
+            if event.type == p.KEYDOWN:
+                if event.key == p.K_ESCAPE:
+                    sys.exit()
+
+    def button_play_pressed(self):
+        return True
+
+
+if __name__ == '__main__':
+    menu = MainMenu()
+    menu.main_loop()
