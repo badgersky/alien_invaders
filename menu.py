0788016864471708ebb73ca260979d90aada7d42
badgersky <szymonek.ptasznik@gmail.com>
2023-03-27 22:09:54 +0200

    Changed menu classes, now MainMenu inherits after Menu class

--- menu.py
+++ menu.py
@@ -6,9 +6,9 @@ from star import Star
 from text import Text
 
 
-class MainMenu:
+class Menu:
 
-    def __init__(self):
+    def __init__(self, title_img):
         p.init()
         self.screen = p.display.set_mode((1080, 720))
         self.width = self.screen.get_width()
@@ -16,13 +16,12 @@ class MainMenu:
         p.display.set_caption('Empire Invaders')
         self.stars = p.sprite.Group()
 
-        title_img = p.image.load('images/title.bmp')
         play_button_img = p.image.load('images/play_button.bmp')
         quit_button_img = p.image.load('images/quit_button.bmp')
 
+        self.title = Text(self.screen, title_img, self.width // 2, self.height // 4)
         self.quit_button = Text(self.screen, quit_button_img, self.width // 2, self.height // 1.3)
         self.play_button = Text(self.screen, play_button_img, self.width // 2, self.height // 1.7)
-        self.title = Text(self.screen, title_img, self.width // 2, self.height // 4)
 
     def main_loop(self):
         self.create_stars()
@@ -51,6 +50,24 @@ class MainMenu:
         for _ in range(500):
             new_star = Star(self.screen)
             self.stars.add(new_star)
+
+
+class MainMenu(Menu):
+
+    def __init__(self):
+        title_img = p.image.load('images/title.bmp')
+        super().__init__(title_img)
+
+    def main_loop(self):
+        super().main_loop()
+
+
+class LoseMenu(Menu):
+    pass
+
+
+class WinMenu(Menu):
+    pass
 
 
 if __name__ == '__main__':
